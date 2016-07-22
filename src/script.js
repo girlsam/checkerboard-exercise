df575e8d83ffe75c56b64e0f2fd63b2a21c637f6
Sam Goldsmith <samggold@gmail.com>
2016-07-22 16:11:57 -0600

    update to changing colors

--- src/script.js
+++ src/script.js
@@ -5,13 +5,15 @@ for (var i = 0; i < 9; i++) {
     for (var j = 0; j < 9; j++) {
       var square = document.createElement('div');
         if (i % 2 === j % 2) {
-          square.style.backgroundColor = 'black';
+          square.style.backgroundColor = getRandomColor();
+          //square.style.backgroundColor = 'black';
           square.id = 'square';
           square.style.width = '11.1%';
           square.style.paddingBottom = '11.1%';
           square.style.cssFloat = 'left';
         } else {
-          square.style.backgroundColor = 'red';
+          square.style.backgroundColor = getRandomColor();
+          //square.style.backgroundColor = 'red';
           square.id = 'square';
           square.style.width = '11.1%';
           square.style.paddingBottom = '11.1%';
@@ -21,4 +23,17 @@ for (var i = 0; i < 9; i++) {
     }
   board.appendChild(row);
 }
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 document.body.appendChild(board);
+document.body.appendChild(board);
+setTimeout(function(){
+   window.location.reload(1);
+}, 2000);
+setTimeout();
